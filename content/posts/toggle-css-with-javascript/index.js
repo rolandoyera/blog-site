04aeb8aebd0208d79c6bb6ca7104a550dc223ff8
Rolando Yera <rolysemail@gmail.com>
2021-02-27 19:09:42 -0500

    Everything working

--- content/posts/toggle-css-with-javascript/index.js
+++ content/posts/toggle-css-with-javascript/index.js
@@ -1,22 +1,18 @@
 import React from "react"
 import styles from './index.module.css'
+import { Button } from 'theme-ui'
 
-const ToolTip = () => {
+const Btn = () => {
   return (
     <div>
       <div className="row">
         <div className="col">
-          <button className={styles.btn} tooltip="Tooltip up here!" flow="up">Up</button>
-          <button className={styles.btn} tooltip="Tooltip down way over here!"
-            flow="down">Down</button>
-          <button className={styles.btn} tooltip="This one is a lefty with a really log sentence!"
-            flow="left">Left</button>
-          <button className={styles.btn} tooltip="Tooltip right"
-            flow="right">Right</button>
+          <Button mr={2}>Beep</Button>
+
         </div>
       </div>
     </div>
   )
 }
 
-export default ToolTip
+export default Btn
--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,10 @@ module.exports = {
             title: `About`,
             slug: `/about`,
           },
+          {
+            title: `Contact`,
+            slug: `/contact`,
+          },
         ],
         externalLinks: [
           {
